47f8c8bb0fe9dfa0b9117629261cea0aca0be5d7
Kauan Vaz <kauanvazc@gmail.com>
2023-06-08 11:50:21 -0300

    SELECT feito

--- main.py
+++ main.py
@@ -38,28 +38,82 @@ def S():
     global token
     if token.upper() == 'create'.upper():
         token = nextToken()
-        I()
-    elif token.upper() == 'truncate'.upper():
+        create()
+    elif token.upper() == 'use'.upper():
         token = nextToken()
-        I()
+        ID()
+        if token != ';':
+            messageError(token, ';')
     elif token.upper() == 'insert'.upper():
         token = nextToken()
         if token.upper() != 'into'.upper():
             messageError(token,'into')
         token = nextToken()
         ID()
-        F_()
+        idParenthesesNoType()
         if token.upper() != 'values'.upper():
             messageError(token,'values')
         token = nextToken()
-        V()
-    elif token.upper() == 'use'.upper():
+        valueParentheses()
+    elif token.upper() == 'select'.upper():
         token = nextToken()
-        ID()
+        if token == '*':
+            token = nextToken()
+            fromID()
+        else:
+            ID()
+            idNoParentheses()
+            fromID()
+        where()
+        orderBy()
         if token != ';':
             messageError(token, ';')
+    elif token.upper() == 'truncate'.upper():
+        token = nextToken()
+        create()
 
-def I():
+def where():
+    global token
+
+    if token.upper() == 'WHERE':
+        token = nextToken()
+        ID()
+        
+        if token != "=":
+            messageError(token, "=")
+        token = nextToken()
+        value()
+
+def value():
+    global token
+    if re.search(r"^((\"[^\"]*\")|(\'[^']*\')|([0-9]+))$", token):
+        token = nextToken()
+    else:
+        messageError(token, 'valor')
+
+
+def orderBy():
+    global token
+    if token.upper() == 'ORDER':
+        token = nextToken()
+        if token.upper() != 'BY':
+            messageError(token, "BY")
+        else:
+            token = nextToken()
+            ID()
+            idNoParentheses()
+            if token.upper() in ['DESC', 'ASC']:
+                token = nextToken()
+
+def fromID():
+    global token
+    if token.upper() != "FROM":
+        messageError(token, 'FROM')
+    else:
+        token = nextToken()
+        ID()
+
+def create():
     global token
     if token.upper() == 'database'.upper():
         token = nextToken()
@@ -69,20 +123,20 @@ def I():
     elif token.upper() == 'table'.upper():
         token = nextToken()
         ID()
-        F()
+        idParenthesesWithType()
         if token != ';':
             messageError(token,';')
     else:
         messageError(token,'DATABASE ou TABLE')
 
-def F():
+def idParenthesesWithType():
     global token
-    if token.upper() == '(':
+    if token == '(':
         token = nextToken()
         ID()
         ID()
-        F()
-        if token.upper() != ')':
+        idParenthesesWithType()
+        if token != ')':
             messageError(token,')')
         else:
             token = nextToken()
@@ -90,37 +144,45 @@ def F():
         token = nextToken()
         ID()
         ID()
-        F()
+        idParenthesesWithType()
 
-def F_():
+def idParenthesesNoType():
+    global token
+    if token == '(':
+        token = nextToken()
+        ID()
+        idParenthesesNoType()
+        if token != ')':
+            messageError(token,')')
+        else:
+            token = nextToken()
+    if token == ',':
+        token = nextToken()
+        ID()
+        idParenthesesNoType()
+
+def idNoParentheses():
+    global token
+    if token.upper() == ',':
+        token = nextToken()
+        ID()
+        idNoParentheses()
+    
+
+def valueParentheses():
     global token
     if token.upper() == '(':
         token = nextToken()
-        ID()
-        F_()
+        value()
+        valueParentheses()
         if token.upper() != ')':
             messageError(token,')')
         else:
             token = nextToken()
     if token.upper() == ',':
         token = nextToken()
-        ID()
-        F_()
+        value()
+        valueParentheses()
 
-def V():
-    global token
-    if token.upper() == '(':
-        token = nextToken()
-        token = nextToken()
-        V()
-        if token.upper() != ')':
-            messageError(token,')')
-        else:
-            token = nextToken()
-    if token.upper() == ',':
-        token = nextToken()
-        token = nextToken()
-        V()
-
-
-S()
+if __name__ == "__main__":
+    S()
