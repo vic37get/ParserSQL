ec39699c79b54cb407a6f124c47d0b23d9c92b03
Victor <victorribeiro172@gmail.com>
2023-06-08 17:59:48 -0300

    falta só o ajax

--- parserSQL/urls.py
+++ parserSQL/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'parserSQL'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('save/', views.save_text_file, name='save_text_file'),
+    path('inicia_parser/', views.save_text_file, name='inicia_parser'),
 ]
--- parserSQL/views.py
+++ parserSQL/views.py
@@ -2,23 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 import subprocess
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def index(request):
     dados = ''
-    if request.method == 'POST':
-        uploaded_file = request.FILES['file']
-        dados = uploaded_file.read().decode('utf-8')
     try:
-        saida = subprocess.run(["python3", "main.py", 'input.txt'], capture_output=True, text=True)
+        saida = subprocess.run(["python3", "main.py"], capture_output=True, text=True)
         if saida.returncode != 0:
             dados = saida.stderr.split('TypeError:')[1]
         else:
-            dados = 'Exit sucess 0'
+            dados = 'Exit success 0'
     except subprocess.CalledProcessError as e:
-        ...
+        pass
+
     context = {
         'saidaLog': dados
     }
+    print(dados)
     template = loader.get_template('parserSQL/index.html')
     return HttpResponse(template.render(context, request))
+
+@csrf_exempt
+def save_text_file(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        with open('input.txt', 'w') as f:
+            f.write(content)
+        # Lógica para salvar o conteúdo em um arquivo ou banco de dados
+        # ...
+        return HttpResponse('Arquivo salvo com sucesso!')
+    return HttpResponse('Erro: método não permitido.')
