393b97d1ffd87611f02920b162c3a8746a09dd32
Jasson Silva <jassonjcs11@gmail.com>
2023-06-15 19:42:25 -0300

    alteração na view

--- parserSQL/views.py
+++ parserSQL/views.py
@@ -3,44 +3,48 @@ from django.template import loader
 import subprocess
 from django.views.decorators.csrf import csrf_exempt
 
+
 @csrf_exempt
 def index(request):
-    dados = ''
-    dados = realizaParser() 
-    context = {
-        'saidaLog': dados
-    }
-    template = loader.get_template('parserSQL/index.html')
+    dados = ""
+    dados = realizaParser()
+    context = {"saidaLog": dados}
+    template = loader.get_template("parserSQL/index.html")
     return HttpResponse(template.render(context, request))
+
 
 @csrf_exempt
 def save_text_file(request):
-    if request.method == 'POST':
-        content = request.POST.get('content')
-        with open('input.txt', 'w') as f:
+    if request.method == "POST":
+        content = request.POST.get("content")
+        with open("input.txt", "w") as f:
             f.write(content)
-        dados = realizaParser() 
+        dados = realizaParser()
         return HttpResponse(dados)
-    return HttpResponse('Erro: método não permitido.')
+    return HttpResponse("Erro: método não permitido.")
+
 
 def realizaParser():
     try:
-        saida = subprocess.run(["python3", "main.py"], capture_output=True, text=True)
+        saida = subprocess.run(
+            ["python3", "MyParser.py"], capture_output=True, text=True
+        )
         if saida.returncode != 0:
-            dados = saida.stderr.split('TypeError:')[1]
+            dados = saida.stderr.split("TypeError:")[1]
         else:
-            dados = 'Exit success 0'
+            dados = "Exit success 0"
     except subprocess.CalledProcessError as e:
         pass
     return dados
 
+
 def splitMantendoSeparador(string, separador):
     string_split = []
-    string_aux = ''
+    string_aux = ""
     for caractere in string:
         string_aux += caractere
         if caractere == separador:
             string_split.append(string_aux)
-            string_aux = ''
+            string_aux = ""
     string_split.append(string_aux)
     return string_split
