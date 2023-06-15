b625c1aa48014dd03b348e4198b0ace54cad2605
MarceloJanio <marcelojanio@ufpi.edu.br>
2023-06-15 16:46:49 -0300

    correcao atualizacao div

--- parserSQL/views.py
+++ parserSQL/views.py
@@ -4,12 +4,14 @@ from django.template import loader
 import subprocess
 from django.views.decorators.csrf import csrf_exempt
 
+context_global = ''
+
 # Create your views here.
 @csrf_exempt
 def index(request):
     dados = ''
     try:
-        saida = subprocess.run(["python3", "main.py"], capture_output=True, text=True)
+        saida = subprocess.run(["python", "main.py"], capture_output=True, text=True)
         if saida.returncode != 0:
             dados = saida.stderr.split('TypeError:')[1]
         else:
@@ -20,7 +22,6 @@ def index(request):
     context = {
         'saidaLog': dados
     }
-    print(dados)
     template = loader.get_template('parserSQL/index.html')
     return HttpResponse(template.render(context, request))
 
@@ -30,7 +31,13 @@ def save_text_file(request):
         content = request.POST.get('content')
         with open('input.txt', 'w') as f:
             f.write(content)
-        # Lógica para salvar o conteúdo em um arquivo ou banco de dados
-        # ...
-        return HttpResponse('Arquivo salvo com sucesso!')
+        try:
+            saida = subprocess.run(["python", "main.py"], capture_output=True, text=True)
+            if saida.returncode != 0:
+                dados = saida.stderr.split('TypeError:')[1]
+            else:
+                dados = 'Exit success 0'
+        except subprocess.CalledProcessError as e:
+            pass
+        return HttpResponse(dados)
     return HttpResponse('Erro: método não permitido.')
